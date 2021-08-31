2927a7e1bb627748aa1979013d3a2220ad2245e7
Ekaterina Potapova <ekaterina_potapova@epam.ru>
2021-08-31 19:37:36 +0300

    commit format

--- src/test/java/tests/CustomWaitCondition.java
+++ src/test/java/tests/CustomWaitCondition.java
@@ -7,13 +7,12 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 public class CustomWaitCondition {
 
     public static ExpectedCondition<Boolean> jQueryAJAXsCompleted() {
-        return new ExpectedCondition<Boolean>(){
+
+       return new ExpectedCondition<Boolean>(){
             public Boolean apply(WebDriver driver){
                 return ((JavascriptExecutor)driver).executeScript("return document.readyState").equals("complete");
                 //Длем когда свойство Document.readyState станет complete
                 // Это значит что страница загружена вместе с дополнительными ресурсами.
-               // такой  из лекции : return (Boolean) ((JavascriptExecutor)driver).executeScript("return
-                // (window.jQuery != null) && (jQuery.active === 0);"); не работает
             }
         };
     }
